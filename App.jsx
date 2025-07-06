69df75da22fb6ec7e8227d46fbd0a0e1baf55df3
JUNSEOKLEE <84374656+yun12154@users.noreply.github.com>
2025-07-06 16:34:08 +0900

    first commit with .gitignore

---
+++ App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-500">
+        Vite + React + Tailwind 성공!
+      </h1>
+    </main>
+  );
+}
